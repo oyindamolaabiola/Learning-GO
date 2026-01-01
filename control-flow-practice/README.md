ac59ef8fed2e667e16606e94eae51c30768a2d57
miield <aodasola95@gmail.com>
2026-01-01 17:57:56 +0100

    Remove submodule and track control-flow-practice as normal folder

---
+++ control-flow-practice/README.md
@@ -0,0 +1,9 @@
+// Task: Build a CLI Calculator
+
+// Requirements:
+// Program should keep running until the user types quit.
+// Each loop: ask for two numbers and an operator (+, -, *, /).
+// Use fmt.Scanln to read the inputs.
+// Perform the calculation and print the result.
+// Handle invalid inputs gracefully (e.g., operator not recognized).
+
---
+++ control-flow-practice/nested_loop.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// // Print a 4 × 5 grid of *
+	// for a := 1; a <= 4; a++ {
+	// 	for b := 1; b <= 5; b++ {
+	// 		fmt.Printf("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// // Print right angle triangle
+	// for row := 1; row <= 4; row++ {
+	// 	for star := 1; star <= row; star++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// // Print inverted right angle triangle
+	// for row := 1; row < 5; row++ {
+	// 	for star := 1; star <= 5-row; star++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// // Print a pyramid of height 4
+	// for row := 1; row < 5; row++ {
+	// 	for space := 1; space <= 5-row; space++ {
+	// 		fmt.Print(" ")
+	// 	}
+	// 	for star := 1; star <= row; star++ {
+	// 			fmt.Print("*")
+	// 		}
+
+	// 	for star := 1; star < row; star++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// Print a 4×4 checkerboard using X and O. 
+	// Conditions based on (row + col) % 2
+	for row := 1; row <= 4; row++ {
+		for col := 1; col <= 4; col++ {
+			if (row + col) % 2 == 0 {
+				fmt.Print("X")
+			} else {
+				fmt.Print(0)
+			}
+		}
+		fmt.Println()
+	}
+
+	
+
+}
