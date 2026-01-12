e4bd280cdcf31ceb075535857431ade5c42ba883
miield <aodasola95@gmail.com>
2026-01-12 20:47:05 +0100

    create cli

--- MiniProjects/go_cli_task.go
+++ MiniProjects/go_cli_task.go
@@ -2,12 +2,39 @@ package main
 
 import (
 	"fmt"
-	// "os"
+	"os"
+
+	"go-cli-tasks/internal/task"
 )
 
+// func main() {
+// 	// if len(os.Args) < 2 {
+// 		fmt.Println("Usage: task [add|list]")
+// 		return
+// 	// }
+// }
+
 func main() {
-	// if len(os.Args) < 2 {
-		fmt.Println("Usage: task [add|list]")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: task [add|list|complete]")
 		return
-	// }
+	}
+
+	command := os.Args[1]
+
+	service := task.NewService()
+
+	switch command {
+	case "add":
+		fmt.Println("add command received")
+		// service.Add(...)
+	case "list":
+		fmt.Println("list command received")
+		// service.List()
+	case "complete":
+		fmt.Println("complete command received")
+		// service.Complete(...)
+	default:
+		fmt.Println("unknown command:", command)
+	}
 }
